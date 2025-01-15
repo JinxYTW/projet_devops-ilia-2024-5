55bb058aaad96d6d7325673fbbd2774c2e19aa19
Nour_Eddine-T <noureddinetaleb.mail@gmail.com>
2025-01-15 22:48:31 +0100

    Implémentation du module 'flask_jwt_extended' pour la gestion des tokens

--- microserv_API_authentification/main.py
+++ microserv_API_authentification/main.py
@@ -1,16 +1,22 @@
 from flask import Flask, request, jsonify
-import uuid
+from flask_jwt_extended import JWTManager
+from flask_jwt_extended import create_access_token
+from datetime import timedelta
+import os
+from dotenv import load_dotenv
+
+# Charger les variables d'environnement du fichier .env
+load_dotenv()
 
 app = Flask(__name__)
 
-# Base de données
+# Configuration du secret pour JWT
+app.config["JWT_SECRET_KEY"] = os.getenv("JWT_SECRET_KEY")
+app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(hours=1)
+jwt = JWTManager(app)
+
+# Base de données simulée
 users_db = {}
-tokens_db = {}
-
-
-# Token
-def generate_token():
-    return str(uuid.uuid4())
 
 
 # Endpoint pour l'inscription
@@ -24,6 +30,9 @@ def sign_in():
     username = data.get("username")
     pseudo = data.get("pseudo")
 
+    if not all([nom, prenom, email, password, username, pseudo]):
+        return jsonify({"message": "Tous les champs sont obligatoires"}), 400
+
     if username in users_db:
         return jsonify({"message": "Nom d'utilisateur déjà pris"}), 400
 
@@ -33,13 +42,12 @@ def sign_in():
         "email": email,
         "password": password,
         "username": username,
-        "pseudo": pseudo
+        "pseudo": pseudo,
     }
 
     users_db[username] = user
-    token = generate_token()
-    tokens_db[token] = username
-    return jsonify({"token": token}), 200
+    access_token = create_access_token(identity=username)
+    return jsonify({"token": access_token}), 200
 
 
 # Endpoint pour la connexion
@@ -49,12 +57,16 @@ def login():
     username_or_email = data.get("username_or_email")
     password = data.get("password")
 
+    if not username_or_email or not password:
+        return jsonify({"message": "Nom d'utilisateur ou mot de passe requis"}), 400
+
     for user in users_db.values():
-        if ((user["email"] == username_or_email or user["username"] == username_or_email) and
-                user["password"] == password):
-            token = generate_token()
-            tokens_db[token] = user["username"]
-            return jsonify({"token": token}), 200
+        if (
+                (user["email"] == username_or_email or user["username"] == username_or_email)
+                and user["password"] == password
+        ):
+            access_token = create_access_token(identity=user["username"])
+            return jsonify({"token": access_token}), 200
 
     return jsonify({"message": "Nom d'utilisateur ou mot de passe incorrect"}), 400
 
