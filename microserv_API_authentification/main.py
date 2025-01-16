14b733e17844acf1fff16655973b37cadbfd1e27
Druhet joachim <66328347+jonitreak@users.noreply.github.com>
2025-01-16 17:08:30 +0100

    Correction faute de frappe

--- microserv_API_authentification/main.py
+++ microserv_API_authentification/main.py
@@ -15,10 +15,10 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL", "sqlite:///app.db")  # URL à modifier 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['JWT_SECRET_KEY'] = os.getenv("JWT_SECRET_KEY", "super-secret")
-app.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(minutes=15)  # 15 minutes
-app.config['JWT_REFRESH_TOKEN_EXPIRES'] = timedelta(days=1)  # Session maximale d'une journée
-app.config['JWT_TOKEN_LOCATION'] = ['headers']  # Permet de vérifier les tokens dans les headers
-app.config['JWT_COOKIE_CSRF_PROTECT'] = False  # Désactiver la protection CSRF si inutile pour API publique
+app.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(minutes=15) 
+app.config['JWT_REFRESH_TOKEN_EXPIRES'] = timedelta(days=1)  
+app.config['JWT_TOKEN_LOCATION'] = ['headers'] 
+app.config['JWT_COOKIE_CSRF_PROTECT'] = False 
 
 db = SQLAlchemy(app)
 jwt = JWTManager(app)
