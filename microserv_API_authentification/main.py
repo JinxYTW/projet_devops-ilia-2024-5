4e5bbb3269d967e12ec94a6cea3a561b3a07026a
Nour_Eddine-T <noureddinetaleb.mail@gmail.com>
2025-01-17 22:23:33 +0100

    Refactor: code cleaning

--- microserv_API_authentification/main.py
+++ microserv_API_authentification/main.py
@@ -1,36 +1,33 @@
 from flask import Flask, request, jsonify
-from flask_jwt_extended import JWTManager, create_access_token
+from flask_jwt_extended import JWTManager
 from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
 from flask_sqlalchemy import SQLAlchemy
 from datetime import timedelta
 import os
 from dotenv import load_dotenv
 
-
-
 load_dotenv()
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL", "sqlite:///app.db")  # URL à modifier 
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL", "DATABASE_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['JWT_SECRET_KEY'] = os.getenv("JWT_SECRET_KEY", "super-secret")
-app.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(minutes=15) 
-app.config['JWT_REFRESH_TOKEN_EXPIRES'] = timedelta(days=1)  
-app.config['JWT_TOKEN_LOCATION'] = ['headers'] 
-app.config['JWT_COOKIE_CSRF_PROTECT'] = False 
+app.config['JWT_SECRET_KEY'] = os.getenv("JWT_SECRET_KEY", "JWT_SECRET_KEY")
+app.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(minutes=15)
+app.config['JWT_REFRESH_TOKEN_EXPIRES'] = timedelta(days=1)
+app.config['JWT_TOKEN_LOCATION'] = ['headers']
+app.config['JWT_COOKIE_CSRF_PROTECT'] = False
 
 db = SQLAlchemy(app)
 jwt = JWTManager(app)
 
 
-# Callback pour rafraîchir automatiquement les tokens à chaque requête
 @jwt_required()
 def refresh_token_if_needed():
     current_user = get_jwt_identity()
-    # Réémettre un token d'accès
     new_token = create_access_token(identity=current_user)
     return jsonify({"new_token": new_token}), 200
+
 
 class User(db.Model):
     __tablename__ = 'users'
@@ -50,19 +47,21 @@ class User(db.Model):
             "username": self.username,
             "pseudo": self.pseudo
         }
-    
+
     def basic_info(self):
         return {
-            "username":self.username,
-            "pseudo":self.pseudo
+            "username": self.username,
+            "pseudo": self.pseudo
         }
-    
-#--------------SIGN IN--------------------
 
 
+# Endpoint pour l'incription
 @app.route("/auth/sign_in", methods=["POST"])
 def sign_in():
     data = request.get_json()
+    if not data:
+        return jsonify({"message": "Données manquantes"}), 400
+
     nom = data.get("nom")
     prenom = data.get("prenom")
     email = data.get("email")
@@ -71,11 +70,10 @@ def sign_in():
     pseudo = data.get("pseudo")
 
     if not all([nom, prenom, email, password, username, pseudo]):
-        return jsonify({"message": "Tous les champs sont obligatoires"}), 401
-    if User.query.filter(User.username == username).first():
-        return jsonify({"message": "Nom d'utilisateur déjà pris"}), 401
-    if User.query.filter(User.email == email).first():
-        return jsonify({"message": "Email déjà pris"}), 401
+        return jsonify({"message": "Tous les champs sont obligatoires"}), 400
+
+    if User.query.filter_by(username=username).first() or User.query.filter_by(email=email).first():
+        return jsonify({"message": "Nom d'utilisateur ou email déjà utilisé"}), 409
 
     new_user = User(nom=nom, prenom=prenom, email=email, password=password, username=username, pseudo=pseudo)
     db.session.add(new_user)
@@ -85,9 +83,7 @@ def sign_in():
     return jsonify({"token": access_token}), 200
 
 
-#--------------LOGIN--------------------
-
-
+# Endpoint pour la connexion
 @app.route("/auth/login", methods=["POST"])
 def login():
     data = request.get_json()
@@ -95,7 +91,7 @@ def login():
     password = data.get("password")
 
     if not username_or_email or not password:
-        return jsonify({"message": "Nom d'utilisateur ou mot de passe requis"}), 401
+        return jsonify({"message": "Nom d'utilisateur ou mot de passe requis"}), 400
 
     user = User.query.filter((User.username == username_or_email) | (User.email == username_or_email)).first()
     if user and user.password == password:
@@ -105,17 +101,29 @@ def login():
     return jsonify({"message": "Nom d'utilisateur ou mot de passe incorrect"}), 401
 
 
-@app.route("/users/<userName>", methods=["GET"])
-def get_user(userName):
-    user = User.query.filter(User.username == userName).first()
+# Endpoint pour récupérer tous les informations d'un utilisateur
+@app.route("/users/<username>", methods=["GET"])
+def get_user(username):
+    user = User.query.filter(User.username == username).first()
     if not user:
         return jsonify({"message": "Utilisateur non trouvé"}), 404
     return jsonify(user.to_dict()), 200
 
 
-@app.route("/users/<userName>", methods=["Delete"])
-def delete_user(userName):
-    user = User.query.filter(User.username == userName).first()
+# Endpoint pour récupérer le nom et le pseudo d'un utilisateur
+@app.route("/users/<username>", methods=["GET"])
+def get_user(username):
+    user = User.query.filter_by(username=username).first()
+    if not user:
+        return jsonify({"message": "Utilisateur non trouvé"}), 404
+
+    return jsonify(user.basic_info()), 200
+
+
+# Endpoint pour supprimer un utilisateur
+@app.route("/users/<username>", methods=["Delete"])
+def delete_user(username):
+    user = User.query.filter(User.username == username).first()
     if not user:
         return jsonify({"message": "Utilisateur non trouvé"}), 404
     db.session.delete(user)
@@ -123,9 +131,10 @@ def delete_user(userName):
     return jsonify({"message": "Utilisateur supprimé"}), 200
 
 
-@app.route("/users/<userName>", methods=["PUT"])
-def update_user(userName):
-    user = User.query.filter(User.username == userName).first()
+# Endpoint pour mettre à jour les informations de l'utilisateur
+@app.route("/users/<username>", methods=["PUT"])
+def update_user(username):
+    user = User.query.filter(User.username == username).first()
     if not user:
         return jsonify({"message": "Utilisateur non trouvé"}), 404
     data = request.get_json()
@@ -138,21 +147,11 @@ def update_user(userName):
     db.session.commit()
     return jsonify(user.to_dict()), 200
 
-#--------------GET INFO--------------------
-@app.route("/users/<string:username>", methods=["GET"])
-def get_user(username):
-    user = User.query.filter_by(username=username).first()
-    if not user:
-        return jsonify({"message": "Utilisateur non trouvé"}), 404
 
-    return jsonify(user.basic_info()), 200
-
-#--------------GET PRIVATE INFO--------------------
-
-@app.route("/users/information/<string:username>", methods=["GET"])
-@jwt_required() 
+# Endpoint pour récupérer
+@app.route("/users/information/<username>", methods=["GET"])
+@jwt_required()
 def get_user_info(username):
-    
     current_user = get_jwt_identity()
     if current_user != username:
         return jsonify({"message": "Accès non autorisé"}), 403
@@ -164,12 +163,10 @@ def get_user_info(username):
     return jsonify(user.to_dict()), 200
 
 
-#--------------DELETE-------------------- 
-
-@app.route("/users/<string:username>", methods=["DELETE"])
-@jwt_required() 
+# Endpoint pour supprimer un utilisateur
+@app.route("/users/<username>", methods=["DELETE"])
+@jwt_required()
 def delete_user(username):
-
     current_user = get_jwt_identity()
     if current_user != username:
         return jsonify({"message": "Accès non autorisé"}), 403
@@ -182,15 +179,15 @@ def delete_user(username):
     db.session.commit()
     return jsonify({"message": "Utilisateur supprimé"}), 200
 
-#--------------UPDATE--------------------
 
-@app.route("/users/<string:username>", methods=["PUT"])
-@jwt_required() 
+# Endpoint pour mettre à jour les informations de l'utilisateur
+@app.route("/users/<username>", methods=["PUT"])
+@jwt_required()
 def update_user(username):
     current_user = get_jwt_identity()
     if current_user != username:
         return jsonify({"message": "Accès non autorisé"}), 403
-    
+
     data = request.get_json()
 
     user = User.query.filter_by(username=username).first()
@@ -206,6 +203,7 @@ def update_user(username):
 
     db.session.commit()
     return jsonify(user.to_dict()), 200
+
 
 if __name__ == "__main__":
     app.run(debug=True)
