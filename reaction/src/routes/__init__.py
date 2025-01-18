3ffb0996e435ee1ed42591c3bd5fc45880cda130
anna <kapandeanna8@gmail.com>
2025-01-18 15:59:16 +0100

    Création de la route de récupération des commentaires de tweet

--- reaction/src/routes/__init__.py
+++ reaction/src/routes/__init__.py
@@ -1,5 +1,6 @@
 from routes.get_reactions_route import tweet_reaction_bp
 from routes.comment_route import comment_bp
 from routes.supp_comments_route import supp_comment_bp
+from routes.comment_get_route import comment_get_bp
 
-blueprints =[ tweet_reaction_bp, comment_bp,supp_comment_bp ]
+blueprints =[ tweet_reaction_bp, comment_bp,supp_comment_bp,comment_get_bp]
