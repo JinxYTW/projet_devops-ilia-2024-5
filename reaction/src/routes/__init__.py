35163a7ec1fc2454dbf3caa7532572345fa7dfc4
eikayna <mouhidsoukaina1803@gmail.com>
2025-01-19 13:02:29 +0100

    Ajouter une reaction tweet

--- reaction/src/routes/__init__.py
+++ reaction/src/routes/__init__.py
@@ -1,5 +1,4 @@
 from routes.get_reactions_route import tweet_reaction_bp
 from routes.comment_route import comment_bp
-from routes.supp_comments_route import supp_comment_bp
-
-blueprints =[ tweet_reaction_bp, comment_bp,supp_comment_bp ]
+from  reaction_tweet_route import reaction_bp
+blueprints =[ tweet_reaction_bp, comment_bp,reaction_bp ]
