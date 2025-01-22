feaa9f84918d9189e1e2289ddc64a690da8b2df4
JinxYTW <tyohann974@gmail.com>
2025-01-22 18:13:35 +0100

    Normalement les API ont été implémentées

---
+++ frontend/src/js/message_prv.js
@@ -0,0 +1,6 @@
+import { MessageController } from '../js/controller/message-controller.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+    const controller = new MessageController();
+    controller.init();
+});
