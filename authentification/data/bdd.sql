043fe34f0bf2189282fe0576c3c67ee63b3943c1
Moad_Garrag <garragmouad8@gmail.com>
2025-01-22 21:42:09 +0100

    création de la bdd polytex  dans bdd.sql

--- authentification/data/bdd.sql
+++ authentification/data/bdd.sql
@@ -1,18 +1,18 @@
+CREATE DATABASE IF NOT EXISTS polytex;
 USE polytex;
 
-CREATE TABLE utilisateur (
-    nom_utilisateur VARCHAR(50) NOT NULL PRIMARY KEY,
-    nom VARCHAR(50) NOT NULL,
-    prenom VARCHAR(50) NOT NULL,
+DROP table IF EXISTS users;
+
+CREATE TABLE users (
+    username VARCHAR(50) NOT NULL PRIMARY KEY,
+    last_name VARCHAR(50) NOT NULL,
+    first_name VARCHAR(50) NOT NULL,
     email VARCHAR(100) NOT NULL,
-    mot_de_passe VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
     pseudo VARCHAR(50) NOT NULL
 );
 
-INSERT INTO utilisateur (Nom, Prenom, Email, Mot_de_passe, Nom_utilisateur, Pseudo)
+INSERT INTO users (last_name, first_name, email, password, username, pseudo)
 VALUES
     ('Dupont', 'Jean', 'jean.dupont@example.com', '$2y$10$nrDWDpEgOI0JrMVZs00rrOsnD/dALCYn3T4nKWIX3EyrpNMF81TSe', 'jdupont', 'JeanD'),
     ('Martin', 'Sophie', 'sophie.martin@example.com', '$2y$10$nrDWDpEgOI0JrMVZs00rrOsnD/dALCYn3T4nKWIX3EyrpNMF81TSe', 'smartin', 'SophieM'),
-    ('Durand', 'Luc', 'luc.durand@example.com', '$2y$10$nrDWDpEgOI0JrMVZs00rrOsnD/dALCYn3T4nKWIX3EyrpNMF81TSe', 'ldurand', 'LucD'),
-    ('Moreau', 'Claire', 'claire.moreau@example.com', '$2y$10$nrDWDpEgOI0JrMVZs00rrOsnD/dALCYn3T4nKWIX3EyrpNMF81TSe', 'cmoreau', 'ClaireM'),
-    ('Rousseau', 'Paul', 'paul.rousseau@example.com', '$2y$10$nrDWDpEgOI0JrMVZs00rrOsnD/dALCYn3T4nKWIX3EyrpNMF81TSe', 'prousseau', 'PaulR');
