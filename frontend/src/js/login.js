1b584979b0b81753f6cacf7c8cc57e951650c5e1
Jaurel Fosset <Jaurel_Fosset@etu.u-bourgogne.fr>
2025-01-22 21:03:11 +0100

    Ajoute le javascript pour se connecter et s'inscrire Note : comme je n'ai pas encore le nom de domaine de l'api, j'ai mis le placeholder https://api.example.com/auth/sign_in

---
+++ frontend/src/js/login.js
@@ -0,0 +1,25 @@
+function init()
+{
+    loginButton = document.getElementById("login-button");
+    loginButton.addEventListener("click", onLogin);
+}
+window.addEventListener("load", init);
+
+function onLogin()
+{
+    pseudo = document.getElementById("pseudo-field").innerHTML;
+    password = document.getElementById("password-field").innerHTML;
+
+    login(pseudo, password);
+}
+
+async function login(pseudo, password)
+{
+    let request =
+    {
+        userName: pseudo,
+        password: password
+    };
+
+    await fetch("https://api.example.com/auth/login", {method : "POST", body: JSON.stringify(request)});
+}
---
+++ frontend/src/js/register.js
@@ -0,0 +1,33 @@
+function init()
+{
+    loginButton = document.getElementById("login-button");
+    loginButton.addEventListener("click", onRegister);
+}
+window.addEventListener("load", init);
+
+function onRegister()
+{
+    firstName = document.getElementById("first-name-field").innerHTML;
+    lastName = document.getElementById("last-name-field").innerHTML;
+
+    email = document.getElementById("email-field").innerHTML;
+    pseudo = document.getElementById("pseudo-field").innerHTML;
+
+    password = document.getElementById("password-field").innerHTML;
+
+    register(firstName, lastName, email, pseudo, password);
+}
+
+async function register(firstName, lastName, email, pseudo, password)
+{
+    let request =
+    {
+        firstName: firstName,
+        lastName: lastName,
+        email: email,
+        userName: pseudo,
+        password: password
+    };
+
+    await fetch("https://api.example.com/auth/sign_in", {method : "POST", body: JSON.stringify(request)});
+}
